2bfb64813efab5afccf810f8d4c1de676e5e5170
Robbie Prokop <robbieprokop@gmail.com>
2022-10-29 15:36:15 +0400

    change zip to poastal code

--- app.js
+++ app.js
@@ -7,9 +7,33 @@ const validateName = () => {
   }
   name.classList.remove("is-invalid");
 };
-const validateZip = () => {};
-const validateEmail = () => {};
-const validatePhone = () => {};
+const validateZip = () => {
+  const zip = document.getElementById("zip");
+  const re = /^([A-Z][0-9]){3}$/;
+  if (!re.test(zip.value)) {
+    return zip.classList.add("is-invalid");
+  }
+
+  zip.classList.remove("is-invalid");
+};
+const validateEmail = () => {
+  const email = document.getElementById("email");
+  const re = /^[0-9]{6}$/;
+  if (!re.test(email.value)) {
+    return email.classList.add("is-invalid");
+  }
+
+  email.classList.remove("is-invalid");
+};
+const validatePhone = () => {
+  const phone = document.getElementById("phone");
+  const re = /^[0-9]{10}$/;
+  if (!re.test(phone.value)) {
+    return phone.classList.add("is-invalid");
+  }
+
+  phone.classList.remove("is-invalid");
+};
 
 //create event listeners for form Blur
 document.getElementById("name").addEventListener("blur", validateName);
