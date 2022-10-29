f27207dc384e60b51fafe92b88c045c415cff298
Robbie Prokop <robbieprokop@gmail.com>
2022-10-29 15:29:38 +0400

    create event listeners and empty functions

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-//app.js
+const validateName = () => {
+  const name = document.getElementById("name");
+  const re = /^[a-zA-Z]{2,14}$/i;
+
+  if (!re.test(name.value)) {
+    return name.classList.add("is-invalid");
+  }
+  name.classList.remove("is-invalid");
+};
+const validateZip = () => {};
+const validateEmail = () => {};
+const validatePhone = () => {};
+
+//create event listeners for form Blur
+document.getElementById("name").addEventListener("blur", validateName);
+document.getElementById("zip").addEventListener("blur", validateZip);
+document.getElementById("email").addEventListener("blur", validateEmail);
+document.getElementById("phone").addEventListener("blur", validatePhone);
